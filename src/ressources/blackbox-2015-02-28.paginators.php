e490835c50af7e4f646d2c71c57aa32c4ac27f46
ossistyle <ossistyle@gmail.com>
2015-03-23 20:31:17 +0100

    new paginators config for command operations

---
+++ src/ressources/blackbox-2015-02-28.paginators.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'pagination' => [
+        'GetProducts' => [
+            'limit' => 'Pagination.EntriesPerPage',
+            'page' => 'Pagination.PageNumber',
+            'more_results' => 'Pagination.HasNextPage',
+            'result_key' => 'EntityList'
+        ],
+    ]
+];
