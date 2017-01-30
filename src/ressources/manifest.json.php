5d212e06135d133979c294d5dc10bf3114e8dc7f
Thomas Hoffmann <thoffmann@dus.via.de>
2017-01-30 15:18:23 +0100

    add new model for new webapi

--- src/ressources/manifest.json.php
+++ src/ressources/manifest.json.php
@@ -1,3 +1,3 @@
 <?php
-// This file was auto-generated from sdk-root/var/www/html/vws-php/src/ressources/manifest.json
-return array ( 'wcfapi' => array ( 'namespace' => 'WcfApi', 'versions' => array ( 'latest' => '2015-10-05', '2015-10-05' => '2015-10-05', ), ), 'webapi' => array ( 'namespace' => 'WebApi', 'versions' => array ( 'latest' => '2015-05-12', '2015-05-12' => '2015-05-12', ), ),);
+// This file was auto-generated from sdk-rootC:\xampp\htdocs\vws-php\src\ressources\manifest.json
+return array ( 'wcfapi' => array ( 'namespace' => 'WcfApi', 'versions' => array ( 'latest' => '2015-10-05', '2015-10-05' => '2015-10-05', ), ), 'webapi' => array ( 'namespace' => 'WebApi', 'versions' => array ( 'latest' => '2017-01-30', '2017-01-30' => '2017-01-30', '2015-05-12' => '2015-05-12', ), ),);
