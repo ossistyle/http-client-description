2b7a9a7fb851c14980884e0f2c26156fde184af7
Thomas Hoffmann <thoffmann@dus.via.de>
2017-01-30 15:20:38 +0100

    update model

--- src/ressources/webapi/2015-05-12/paginators.json.php
+++ src/ressources/webapi/2015-05-12/paginators.json.php
@@ -1,3 +1,3 @@
 <?php
-// This file was auto-generated from sdk-root/var/www/html/vws-php/src/ressources/webapi/2015-05-12/paginators.json
+// This file was auto-generated from sdk-rootC:\xampp\htdocs\vws-php\src\ressources\webapi\2015-05-12\paginators.json
 return array ( 'pagination' => array ( 'GetProducts' => array ( 'limit' => 'Pagination.EntriesPerPage', 'page' => 'Pagination.PageNumber', 'more_results' => 'Pagination.HasNextPage', 'result_key' => 'EntityList', ), 'GetNewSalesOrdersInput' => array ( 'limit' => 'Pagination.EntriesPerPage', 'page' => 'Pagination.PageNumber', 'more_results' => 'Pagination.HasNextPage', 'result_key' => 'EntityList', ), ),);
